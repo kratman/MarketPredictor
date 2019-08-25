e4d232c9c3dabf9bce2373518cc5864a4ed0c073
kratman <kratmandu@gmail.com>
2019-08-25 15:55:28 -0400

    Adding a simple uncoupled Monte Carlo asset

--- predictor/dynamics/dynamics.go
+++ predictor/dynamics/dynamics.go
@@ -2,7 +2,7 @@ package dynamics
 
 import "math/rand"
 
-type asset struct {
+type CoupledAsset struct {
 	price float64
 	oldPrice float64
 	drag float64
@@ -12,37 +12,37 @@ type asset struct {
 	fluctuationSize float64
 }
 
-func NewAsset(price float64, drag float64, growth float64, coupling float64, flux float64) asset {
-	return asset {price, price, drag, 0.0, growth, coupling, flux}
+func NewAsset(price float64, drag float64, growth float64, coupling float64, flux float64) CoupledAsset {
+	return CoupledAsset{price, price, drag, 0.0, growth, coupling, flux}
 }
 
-func updateAsset(currentAsset asset, coupledVelocity float64) {
+func updateAsset(currentAsset CoupledAsset, coupledVelocity float64) {
 	updatePrice(currentAsset, coupledVelocity)
 	updateVelocity(currentAsset)
 }
 
-func updatePrice(currentAsset asset, coupledVelocity float64) {
+func updatePrice(currentAsset CoupledAsset, coupledVelocity float64) {
 	currentAsset.oldPrice = currentAsset.price
 	addDynamics(currentAsset)
 	addCoupling(currentAsset, coupledVelocity)
 	addFluctuation(currentAsset)
 }
 
-func addDynamics(currentAsset asset) {
+func addDynamics(currentAsset CoupledAsset) {
 	var velocityUpdate = currentAsset.drag * currentAsset.velocity
 	var growthUpdate = currentAsset.growthRate * currentAsset.price
 	currentAsset.price += velocityUpdate + growthUpdate
 }
 
-func addCoupling(currentAsset asset, coupledVelocity float64) {
+func addCoupling(currentAsset CoupledAsset, coupledVelocity float64) {
 	currentAsset.price += currentAsset.coupledRate * coupledVelocity
 }
 
-func addFluctuation(currentAsset asset) {
+func addFluctuation(currentAsset CoupledAsset) {
 	var scale = 2.0 * rand.Float64() - 1.0
 	currentAsset.price += scale * currentAsset.fluctuationSize
 }
 
-func updateVelocity(currentAsset asset) {
+func updateVelocity(currentAsset CoupledAsset) {
 	currentAsset.velocity = currentAsset.price - currentAsset.oldPrice
 }
---
+++ predictor/montecarlo/montecarlo.go
@@ -0,0 +1,31 @@
+package montecarlo
+
+import (
+	"math"
+	"math/rand"
+)
+
+type SoloAsset struct {
+	price float64
+	mean float64
+	variance float64
+}
+
+func NewAsset(price float64, mean float64, variance float64) SoloAsset {
+	return SoloAsset{price, mean, variance}
+}
+
+func updateMonteCarlo(currentAsset SoloAsset) {
+	var change = calculateDrift(currentAsset)
+	change += calculateJump(currentAsset)
+	currentAsset.price *= math.Exp(change)
+}
+
+func calculateJump(currentAsset SoloAsset) float64 {
+	var scale = 2.0*rand.Float64() - 1.0
+	return currentAsset.variance * scale
+}
+
+func calculateDrift(currentAsset SoloAsset) float64 {
+	return currentAsset.mean - 0.5 * currentAsset.variance
+}
