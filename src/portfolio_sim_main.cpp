531edb89c90299597d5f97817a1201379e89c903
kratman <kratmandu@gmail.com>
2025-10-09 20:31:10 -0400

    Add a main for testing

--- src/portfolio_sim_main.cpp
+++ src/portfolio_sim_main.cpp
@@ -1,10 +1,45 @@
+#include <iostream>
+#include <vector>
+
 #include "brownian_asset.h"
 #include "montecarlo_asset.h"
+using namespace std;
 
 
 int main() {
-    auto monte_asset = SoloAsset();
     auto dynamic_asset = CoupledAsset();
 
+    constexpr double numberOfTrials = 10;
+    constexpr double numberOfSteps = 1000;
+    std::vector<SoloAsset> all_assets;
+
+    for (int i=0; i < numberOfTrials; i++) {
+        constexpr double price = 44.06;
+        constexpr double meanReturn = 0.000284685088744;
+        constexpr double variance = 0.011551333339698;
+        all_assets.emplace_back(price, meanReturn, variance);
+    }
+
+
+    cout << "Step";
+    for (int i=0; i < numberOfTrials; i++) {
+        cout << ", Price " <<  i;
+    }
+    cout << endl;
+
+    cout << 0;
+    for (const auto & an_asset : all_assets) {
+        cout << ", " << an_asset.GetPrice();
+    }
+    cout << endl;
+
+    for (int i=0; i < numberOfSteps; i++) {
+        cout << i+1;
+        for (auto & an_asset : all_assets) {
+            an_asset.UpdateMonteCarlo();
+            cout << ", " << an_asset.GetPrice();
+        }
+        cout << endl;
+    }
     return 0;
 }
